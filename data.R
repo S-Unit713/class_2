a88e903c19158c9aa3cde8e51fc30e6b9db62cf0
Sunit Shukla <sunitshukla713@gmail.com>
2015-07-08 14:50:30 -0400

    dumping dputting R files

---
+++ data.R
@@ -0,0 +1,6 @@
+x <-
+"foo"
+y <-
+structure(list(a = 1, b = structure(1L, .Label = "a", class = "factor"), 
+    c = structure(1L, .Label = "TRUE", class = "factor")), .Names = c("a", 
+"b", "c"), row.names = c(NA, -1L), class = "data.frame")
---
+++ y.R
@@ -0,0 +1,3 @@
+structure(list(a = 1, b = structure(1L, .Label = "a", class = "factor"), 
+    c = structure(1L, .Label = "TRUE", class = "factor")), .Names = c("a", 
+"b", "c"), row.names = c(NA, -1L), class = "data.frame")
