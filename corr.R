4b885d68941805a296dc4e96cb38daac4554873b
Sunit Shukla <sunitshukla713@gmail.com>
2015-07-21 17:05:53 -0400

    Adding assignment files

--- corr.R
+++ corr.R
@@ -2,16 +2,18 @@ corr<- function(directory, threshold= 0){
         directory <- paste("C:/Users/sshukla/class_2",directory, sep="/")        
         setwd(directory)
         data.files <- list.files()
-        
+        good <- NULL
+        cr <- NULL
         obs <- complete("specdata", 1:332)
-        for(j in nrow(obs)){
-                if(obs[j,][]$nobs>threshold){
+        for(j in 1:nrow(obs)){
+                if(obs[j,2] > threshold){
                         
-                        dt <- read.csv(data.files[obs$id], header= TRUE, na.strings= "NA")
-                        good <- rbind(good, complete.cases(dt$date,dt$sulphate,dt$nitrate))
-                                                
+                        dt <- read.csv(data.files[j], header= TRUE, na.strings= "NA")
+                        good <- complete.cases(dt$date,dt$sulphate,dt$nitrate)
+                        df <- dt[good,][]
+                        cr <- list(cr, cor(as.numeric(df[,3]), as.numeric(df[,4])))                                                
                 }
         }
-        cr <- cor(good$sulphate, good$nitrate)
+        
         cr
 }
